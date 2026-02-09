bde3bba239a4bd422ea2cfc6a45a9720d705b7ea
LukaszPilarski <lukasz.pilarski@vonage.com>
2026-02-09 16:24:25 +0100

    Imported OpenTok source into lib/js

--- src/NetworkTest/testQuality/helpers/getLatestSampleWindow.ts
+++
@@ -1,9 +0,0 @@
-import config from './config';
-import { getOr, last } from '../../util';
-import { PublisherStats } from '../../types/publisher';
-
-export default function getLatestSampleWindow(stats: PublisherStats[]): PublisherStats[] {
-  const mostRecentTimestamp: number = getOr(0, 'timestamp', last(stats));
-  const oldestAllowedTime: number = mostRecentTimestamp - config.steadyStateSampleWindow;
-  return stats.filter((stat: PublisherStats) => stat.timestamp >= oldestAllowedTime);
-}
